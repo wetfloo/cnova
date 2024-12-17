ae29c38ee63e367d3f650cf9a622bdea62d41538
wetfloo <yahahaha.1234567890@gmail.com>
2024-12-18 00:57:11 +0700

    big refactor, implementations for conversions

---
+++ src/file.rs
@@ -0,0 +1,114 @@
+use crate::remote::LyricsRequest;
+use crate::util::todo_err;
+use lofty::{
+    file::{TaggedFile, TaggedFileExt},
+    probe::Probe,
+    read_from_path,
+    tag::Accessor,
+};
+use rayon::{iter::IntoParallelRefIterator, prelude::*};
+use std::path::Path;
+use walkdir::{DirEntry, WalkDir};
+
+pub struct DirIterCfg {
+    pub skip_hidden: bool,
+    pub skip_non_music_ext: bool,
+    pub laxed_ext_mode: bool,
+}
+
+impl Default for DirIterCfg {
+    fn default() -> Self {
+        Self {
+            skip_hidden: false,
+            skip_non_music_ext: true,
+            laxed_ext_mode: false,
+        }
+    }
+}
+
+fn files<P>(path: P, cfg: &DirIterCfg) -> Vec<DirEntry>
+where
+    P: AsRef<Path>,
+{
+    WalkDir::new(path)
+        .into_iter()
+        .filter_entry(|entry| !cfg.skip_hidden || entry.is_hidden())
+        .filter_map(|entry_res| entry_res.map_err(|e| todo_err!(e)).ok())
+        .filter(|entry| entry.is_suitable_file(cfg))
+        .collect()
+}
+
+async fn all_file_requests<I>(entries: I, cfg: &DirIterCfg) -> Vec<LyricsRequest>
+where
+    // I still don't get HRTBs....
+    for<'a> I: IntoParallelRefIterator<'a, Item = DirEntry>,
+{
+    entries
+        .par_iter()
+        .filter_map(|entry| {
+            dbg!(&entry);
+            if cfg.laxed_ext_mode {
+                read_from_path(entry.path()).map_err(|e| todo_err!(e))
+            } else {
+                Probe::open(entry.path())
+                    .map_err(|e| todo_err!(e))
+                    .and_then(|probe| probe.guess_file_type().map_err(|e| todo_err!(e)))
+                    .and_then(|probe| probe.read().map_err(|e| todo_err!(e)))
+            }
+            .ok()
+        })
+        .filter_map(prepare_lyrics_request)
+        .collect() // TODO: remove this and send requests
+}
+
+fn prepare_lyrics_request(file: TaggedFile) -> Option<LyricsRequest> {
+    let tags_set = file.tags().first()?;
+    let request = LyricsRequest {
+        artist: tags_set.artist()?.into_owned(),
+        title: tags_set.title()?.into_owned(),
+        album: tags_set.album().map(|cow| cow.into_owned()),
+        duration_secs: None, // TODO
+    };
+    Some(request)
+}
+
+trait DirEntryExt {
+    fn is_hidden(&self) -> bool;
+
+    fn is_suitable_file(&self, cfg: &DirIterCfg) -> bool;
+}
+
+impl DirEntryExt for DirEntry {
+    fn is_hidden(&self) -> bool {
+        self.file_name()
+            .to_str()
+            .map(|s| s.starts_with("."))
+            .unwrap_or(false)
+    }
+
+    fn is_suitable_file(&self, cfg: &DirIterCfg) -> bool {
+        // TODO: this does not follow symlinks, fix it
+        if !self.file_type().is_file() {
+            return false;
+        }
+
+        if !cfg.skip_non_music_ext {
+            return true;
+        }
+
+        match self
+            .path()
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .map(|s| s.to_ascii_lowercase())
+        {
+            Some(extension) => {
+                matches!(
+                    extension.as_str(),
+                    "mp3" | "mp4" | "aac" | "alac" | "flac" | "opus" | "ogg" | "wav"
+                )
+            }
+            None => false,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
-use lofty::{file::TaggedFileExt, probe::Probe, read_from_path, tag::Accessor};
-use rayon::prelude::*;
+use file::DirIterCfg;
 use std::{env, process};
-use util::todo_err;
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 
+mod file;
 mod remote;
 mod util;
-
-struct DirIterCfg {
-    skip_hidden: bool,
-    skip_non_music_ext: bool,
-    laxed_ext_mode: bool,
-}
-
-impl Default for DirIterCfg {
-    fn default() -> Self {
-        Self {
-            skip_hidden: false,
-            skip_non_music_ext: true,
-            laxed_ext_mode: false,
-        }
-    }
-}
 
 fn main() {
     let dir_iter_cfg = DirIterCfg::default();
@@ -32,71 +15,4 @@ fn main() {
     });
 
     let iter = WalkDir::new(file_path).into_iter();
-    let files: Vec<_> = iter
-        .filter_entry(|entry| !dir_iter_cfg.skip_hidden || entry.is_hidden())
-        .filter_map(|entry_res| entry_res.map_err(|e| todo_err!(e)).ok())
-        .filter(|entry| entry.is_suitable_file(&dir_iter_cfg))
-        .collect();
-
-    files.into_par_iter().for_each(|entry| {
-        dbg!(&entry);
-        let tagged_file = if dir_iter_cfg.laxed_ext_mode {
-            read_from_path(entry.path()).map_err(|e| todo_err!(e))
-        } else {
-            Probe::open(entry.path())
-                .map_err(|e| todo_err!(e))
-                .and_then(|probe| probe.guess_file_type().map_err(|e| todo_err!(e)))
-                .and_then(|probe| probe.read().map_err(|e| todo_err!(e)))
-        }
-        .ok();
-
-        match tagged_file {
-            Some(tagged_file) => {
-                for tag in tagged_file.tags() {
-                    dbg!(tag.title(), tag.artist());
-                }
-            }
-            None => eprintln!("TODO: failed to read file"),
-        }
-    })
-}
-
-trait DirEntryExt {
-    fn is_hidden(&self) -> bool;
-
-    fn is_suitable_file(&self, cfg: &DirIterCfg) -> bool;
-}
-
-impl DirEntryExt for DirEntry {
-    fn is_hidden(&self) -> bool {
-        self.file_name()
-            .to_str()
-            .map(|s| s.starts_with("."))
-            .unwrap_or(false)
-    }
-
-    fn is_suitable_file(&self, cfg: &DirIterCfg) -> bool {
-        if !self.file_type().is_file() {
-            return false;
-        }
-
-        if !cfg.skip_non_music_ext {
-            return true;
-        }
-
-        match self
-            .path()
-            .extension()
-            .and_then(|ext| ext.to_str())
-            .map(|s| s.to_ascii_lowercase())
-        {
-            Some(extension) => {
-                matches!(
-                    extension.as_str(),
-                    "mp3" | "mp4" | "aac" | "alac" | "flac" | "opus" | "ogg" | "wav"
-                )
-            }
-            None => false,
-        }
-    }
 }
