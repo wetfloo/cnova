39336c51962c9ebf6e321d01c66d3f394bb4a8b8
wetfloo <yahahaha.1234567890@gmail.com>
2024-12-18 10:32:20 +0700

    finally fix this mess

--- src/remote.rs
+++ src/remote.rs
@@ -14,10 +14,14 @@ static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(|| client_init().unwrap
 
 #[derive(Debug, Serialize)]
 pub struct LyricsRequest {
+    #[serde(rename = "artist_name")]
     pub artist: String,
+    #[serde(rename = "track_name")]
     pub title: String,
+    #[serde(rename = "album_name")]
     pub album: Option<String>,
-    pub duration_secs: Option<u32>,
+    #[serde(rename = "duration")]
+    pub duration_secs: Option<f32>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -28,7 +32,7 @@ pub struct LyricsResponse {
     pub artist_name: Option<String>,
     pub album_name: Option<String>,
     #[serde(rename = "duration")]
-    pub duration_secs: String,
+    pub duration_secs: f32,
     pub instrumental: bool,
     pub plain_lyrics: String,
     pub synced_lyrics: String,
@@ -36,16 +40,39 @@ pub struct LyricsResponse {
 
 #[derive(Debug, thiserror::Error)]
 pub enum LyricsError {
+    #[error("invalid Reqwest request")]
+    InvalidRequest(#[source] reqwest::Error),
     #[error("an error occured")]
     Misc(#[from] reqwest::Error),
+    #[error("invalid status code {status} from url {url}")]
+    InvalidStatusCode {
+        status: reqwest::StatusCode,
+        url: &'static str,
+    },
 }
 
 pub async fn get_lyrics(req: &LyricsRequest) -> Result<LyricsResponse, LyricsError> {
+    let request = CLIENT
+        .get(url::GET)
+        .query(req)
+        .build()
+        .map_err(LyricsError::InvalidRequest)?;
+
     CLIENT
-        .post(url::GET)
-        .form(req)
-        .send()
-        .await?
+        .execute(request)
+        .await
+        .map_err(|e| e.into())
+        .and_then(|response| {
+            let status = response.status();
+            if status.is_success() {
+                Ok(response)
+            } else {
+                Err(LyricsError::InvalidStatusCode {
+                    status,
+                    url: url::GET,
+                })
+            }
+        })?
         .json()
         .await
         .map_err(|e| e.into())
