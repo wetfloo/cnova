1a533b894860cf33ec2f222985133e06158ad0ad
wetfloo <yahahaha.1234567890@gmail.com>
2024-12-21 01:48:26 +0700

    fix broken while let

--- src/main.rs
+++ src/main.rs
@@ -53,38 +53,40 @@ async fn main() {
     let semaphore = Arc::new(tokio::sync::Semaphore::new(cli.download_jobs.into()));
 
     // TODO: MASSIVE bottleneck here. Jobs don't come in until all files are processed, why???
-    while let Some((request, dir_entry)) = rx.recv().await.and_then(|res| res.trace_err().ok()) {
-        tracing::debug!(?request, ?dir_entry, "received new value");
+    while let Some(res) = rx.recv().await {
+        if let Ok((request, dir_entry)) = res.trace_err() {
+            tracing::debug!(?request, ?dir_entry, "received new value");
 
-        let remote = remote.clone();
-        let permit = semaphore.clone().acquire_owned();
+            let remote = remote.clone();
+            let permit = semaphore.clone().acquire_owned();
 
-        join_set.spawn(
-            async move {
-                let permit = permit.await.expect("semaphore closed unexpectedly");
-                let response = remote.get_lyrics(&request).await;
-                drop(permit); // manually drop to handle other tasks in this async block in the future
+            join_set.spawn(
+                async move {
+                    let permit = permit.await.expect("semaphore closed unexpectedly");
+                    let response = remote.get_lyrics(&request).await;
+                    drop(permit); // manually drop to handle other tasks in this async block in the future
 
-                let mut path = dir_entry.into_path();
+                    let mut path = dir_entry.into_path();
 
-                assert!(
-                    path.set_extension("lrc"),
-                    "at this stage, we should always be able to update extensions on files"
-                );
-                match response
-                    .trace_err()
-                    .ok()
-                    .and_then(|response| response.synced_lyrics.or(response.plain_lyrics))
-                {
-                    Some(lyrics) => tokio::fs::write(&path, &lyrics)
-                        .await
-                        .inspect_err(|e| tracing::error!(?e, "failed to write to a file"))
-                        .unwrap_or_default(),
-                    None => tracing::info!(?request, ?path, "couldn\'t extract lyrics"),
+                    assert!(
+                        path.set_extension("lrc"),
+                        "at this stage, we should always be able to update extensions on files"
+                    );
+                    match response
+                        .trace_err()
+                        .ok()
+                        .and_then(|response| response.synced_lyrics.or(response.plain_lyrics))
+                    {
+                        Some(lyrics) => tokio::fs::write(&path, &lyrics)
+                            .await
+                            .inspect_err(|e| tracing::error!(?e, "failed to write to a file"))
+                            .unwrap_or_default(),
+                        None => tracing::info!(?request, ?path, "couldn\'t extract lyrics"),
+                    }
                 }
-            }
-            .in_current_span(),
-        );
+                .in_current_span(),
+            );
+        }
     }
 
     join_set.join_all().await;
