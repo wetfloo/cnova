815fe1da790711ed2c78f359945dbf453bce85ad
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-01 01:19:07 +0500

    basic path traversal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env::home_dir;
+
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!")
+    traverse();
+}
+
+fn traverse() {
+    let mut path = home_dir().unwrap();
+    path.push("Music/Experiment");
+
+    for item in WalkDir::new(&path).into_iter().filter_map(|res| res.ok()) {
+        println!("{:?}", item);
+    }
 }
