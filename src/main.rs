12e178c0a23041942bd75cd0971b2eae4faba86f
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-01 16:13:03 +0500

    add rustfmt.toml, reformat

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ use std::env::home_dir;
 use walkdir::WalkDir;
 
 fn main() {
-    traverse();
+	traverse();
 }
 
 fn traverse() {
-    let mut path = home_dir().unwrap();
-    path.push("Music/Experiment");
+	let mut path = home_dir().unwrap();
+	path.push("Music/Experiment");
 
-    for item in WalkDir::new(&path).into_iter().filter_map(|res| res.ok()) {
-        println!("{:?}", item);
-    }
+	for item in WalkDir::new(&path)
+		.into_iter()
+		.filter_map(|res| res.ok())
+	{
+		println!("{:?}", item);
+	}
 }
