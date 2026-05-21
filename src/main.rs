584ded88892c4b5290a6825c02851c0ca9957e9b
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-21 05:07:53 +0500

    add example lyrics db code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env::home_dir;
+use std::sync::Mutex;
+use std::{env::home_dir, sync::LazyLock};
 
+use sqlite::ffi::sqlite3_stmt_status;
 use walkdir::WalkDir;
 
-fn main() {
-	traverse();
-}
+static LYRICS_DB: LazyLock<sqlite::ConnectionThreadSafe> =
+	LazyLock::new(|| sqlite::Connection::open_thread_safe(":memory:").unwrap());
+
+fn main() {}
 
 fn traverse() {
 	let mut path = home_dir().unwrap();
