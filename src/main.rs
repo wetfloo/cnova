589f8d9493f01983bed7e0dd707f16a036500e88
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-22 03:22:57 +0500

    add more iter tools (heh)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // TODO: remove when we're done.
 #![allow(unused)]
 
+mod iter;
+
 use itertools::Itertools;
 
+use std::iter::Inspect;
 use std::sync::Mutex;
 use std::time::Duration;
 use std::{env::home_dir, sync::LazyLock};
@@ -10,17 +13,39 @@ use std::{env::home_dir, sync::LazyLock};
 use sqlite::ffi::sqlite3_stmt_status;
 use walkdir::WalkDir;
 
-trait IterExt {
-	fn process_err<T, E, F>(self, processor: F) -> impl Iterator<Item = Self::Item>
+struct ProcessError<N> {
+	inner_iter: N,
+}
+
+impl<N> ProcessError<N>
+{
+	fn new<I, F>(iter: I, f: F) -> ProcessError<impl Iterator<Item = I::Item>>
+	where
+		I : Iterator,
+		F: FnMut(&I::Item),
+	{
+		ProcessError {
+			inner_iter: iter.inspect(f),
+		}
+	}
+}
+
+impl<N> Iterator for ProcessError<N>
+where
+	N: Iterator,
+{
+	type Item = N::Item;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.inner_iter.next()
+	}
+}
+
+trait IterExt: Iterator {
+	fn process_err<T, E, F>(self, processor: F) -> ProcessError<impl Iterator<Item = Self::Item>>
 	where
 		Self: Iterator<Item = Result<T, E>> + Sized,
-		F: Fn(&E),
-	{
-		self.inspect(move |res| match res {
-			Ok(_) => (),
-			Err(err) => processor(err),
-		})
-	}
+		F: FnMut(&E);
 
 	fn discard_err<T, E>(self) -> impl Iterator<Item = T>
 	where
@@ -30,7 +55,36 @@ trait IterExt {
 	}
 }
 
-impl<T> IterExt for T where T: Iterator {}
+impl<I> IterExt for I
+where
+	I: Iterator,
+{
+	fn process_err<T, E, F>(
+		self,
+		mut processor: F,
+	) -> ProcessError<impl Iterator<Item = Self::Item>>
+	where
+		Self: Iterator<Item = Result<T, E>> + Sized,
+		F: FnMut(&E),
+	{
+		ProcessError {
+			inner_iter: self.inspect(move |res| {
+				if let Err(err) = res {
+					processor(err)
+				}
+			}),
+		}
+	}
+}
+
+impl<I> DoubleEndedIterator for ProcessError<I>
+where
+	I: DoubleEndedIterator,
+{
+	fn next_back(&mut self) -> Option<Self::Item> {
+		self.inner_iter.next_back()
+	}
+}
 
 trait LyricsHolder {
 	fn lyrics(metadata: &Metadata) -> Option<Lyrics>;
