6d16abdcc3423bb3ad4e67c82614bfa1d1dcf4fd
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-22 22:51:21 +0500

    more of traverse/write code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@
 #![allow(unused)]
 
 use cnova::IterExt;
-
+use lofty::error::LoftyError;
+use lofty::file::TaggedFile as LoftyTaggedFile;
+use lofty::file::{AudioFile as _, TaggedFileExt as _};
+use lofty::probe::Probe as LoftyProbe;
+use lofty::tag::ItemKey as LoftyItemKey;
+use lofty::tag::{Tag as LoftyTag, TagType as LoftyTagType};
+use std::fs::FileType;
+use std::fs::OpenOptions;
 use std::iter::Inspect;
+use std::path::Path;
 use std::sync::Mutex;
 use std::time::Duration;
 use std::{env::home_dir, sync::LazyLock};
@@ -47,14 +55,100 @@ fn traverse() {
 	let mut path = home_dir().unwrap();
 	path.push("Music/Experiment");
 
-	let _: Vec<_> = WalkDir::new(&path)
+	for dir_entry in WalkDir::new(&path)
 		.into_iter()
 		.inspect_err(|err| {
+			// TODO::logging
 			dbg!(err);
 		})
-		.inspect_ok(|val| {
-			dbg!(val);
-		})
 		.discard_err()
-		.collect();
+		.filter(|dir_entry| {
+			dir_entry.file_type().is_file()
+				&& dir_entry
+					.path()
+					.extension()
+					.is_some_and(|ext| ext == "flac")
+		}) {
+		let entry_path = dir_entry.path();
+		match handle_file_guessing(entry_path) {
+			Ok(mut tagged_file) => {
+				for tt in tagged_file.tag_types_to_write() {
+					if let Some(tag) = tagged_file.tag_mut(tt) {
+						dbg!(tag.insert_text(
+							LoftyItemKey::Lyrics,
+							"I've been here before!".to_owned(),
+						));
+					}
+				}
+
+				let mut file = OpenOptions::new()
+					.read(true)
+					.write(true)
+					.open(entry_path)
+					// TODO::unwrap
+					.unwrap();
+				tagged_file
+					// TODO::unwrap
+					.save_to(&mut file, Default::default())
+					.unwrap();
+			},
+
+			Err(err) => {
+				dbg!(&err);
+			},
+		}
+	}
+}
+
+fn handle_file_guessing<P>(path: P) -> Result<LoftyTaggedFile, LoftyError>
+where
+	P: AsRef<Path>,
+{
+	// TODO::config: add a way to make lofty guess (or not) track's filetype.
+	LoftyProbe::open(path)?
+		.guess_file_type()?
+		.read()
+}
+
+struct TagTypesToWrite {
+	tt: LoftyTagType,
+	primary_shown: bool,
+}
+
+impl TagTypesToWrite {
+	fn new(tt: LoftyTagType) -> Self {
+		Self {
+			tt,
+			primary_shown: false,
+		}
+	}
+}
+
+impl Iterator for TagTypesToWrite {
+	type Item = LoftyTagType;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match (self.primary_shown, self.tt) {
+			(false, tt) => {
+				self.primary_shown = true;
+				Some(tt)
+			},
+			(true, LoftyTagType::Id3v2) => Some(LoftyTagType::Id3v1),
+			(true, _) => None,
+		}
+	}
+}
+
+trait TagTypesToWriteExt {
+	type Iter: Iterator<Item = LoftyTagType>;
+
+	fn tag_types_to_write(&self) -> Self::Iter;
+}
+
+impl TagTypesToWriteExt for LoftyTaggedFile {
+	type Iter = TagTypesToWrite;
+
+	fn tag_types_to_write(&self) -> Self::Iter {
+		TagTypesToWrite::new(self.primary_tag_type())
+	}
 }
