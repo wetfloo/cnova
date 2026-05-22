2150c701a8b29abbef31195728aa0d9051373b4d
wetfloo <yahahaha.1234567890@gmail.com>
2026-05-22 07:39:16 +0500

    move impls to trait

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -5,17 +5,23 @@ use crate::iter::discard::{DiscardError, DiscardOk};
 use inspect::{InspectError, InspectOk};
 
 pub trait IterExt: Iterator {
-	/// Allows you to inspect any [Result::Err]'s contents without modifying the iterator.
-	fn inspect_err<T, E, F>(self, inspect: F) -> InspectError<Self, F>
-	where
-		Self: Iterator<Item = Result<T, E>> + Sized,
-		F: FnMut(&E);
-
 	/// Allows you to inspect any [Result::Ok]'s contents without modifying the iterator.
 	fn inspect_ok<T, E, F>(self, inspect: F) -> InspectOk<Self, F>
 	where
 		Self: Iterator<Item = Result<T, E>> + Sized,
-		F: FnMut(&T);
+		F: FnMut(&T),
+	{
+		InspectOk::new(self, inspect)
+	}
+
+	/// Allows you to inspect any [Result::Err]'s contents without modifying the iterator.
+	fn inspect_err<T, E, F>(self, inspect: F) -> InspectError<Self, F>
+	where
+		Self: Iterator<Item = Result<T, E>> + Sized,
+		F: FnMut(&E),
+	{
+		InspectError::new(self, inspect)
+	}
 
 	/// Drops any [Result::Ok] passing along only [Result::Err] inner values.
 	///
@@ -35,7 +41,10 @@ pub trait IterExt: Iterator {
 	/// ```
 	fn discard_ok<T, E>(self) -> DiscardOk<Self>
 	where
-		Self: Iterator<Item = Result<T, E>> + Sized;
+		Self: Iterator<Item = Result<T, E>> + Sized,
+	{
+		DiscardOk::new(self)
+	}
 
 	/// Drops any [Result::Err] passing along only [Result::Ok] inner values.
 	///
@@ -55,40 +64,14 @@ pub trait IterExt: Iterator {
 	/// ```
 	fn discard_err<T, E>(self) -> DiscardError<Self>
 	where
-		Self: Iterator<Item = Result<T, E>> + Sized;
+		Self: Iterator<Item = Result<T, E>> + Sized,
+	{
+		DiscardError::new(self)
+	}
 }
 
 impl<I> IterExt for I
 where
 	I: Iterator,
 {
-	fn inspect_ok<T, E, F>(self, inspect: F) -> InspectOk<Self, F>
-	where
-		Self: Iterator<Item = Result<T, E>> + Sized,
-		F: FnMut(&T),
-	{
-		InspectOk::new(self, inspect)
-	}
-
-	fn inspect_err<T, E, F>(self, inspect: F) -> InspectError<Self, F>
-	where
-		Self: Iterator<Item = Result<T, E>> + Sized,
-		F: FnMut(&E),
-	{
-		InspectError::new(self, inspect)
-	}
-
-	fn discard_ok<T, E>(self) -> DiscardOk<Self>
-	where
-		Self: Iterator<Item = Result<T, E>> + Sized,
-	{
-		DiscardOk::new(self)
-	}
-
-	fn discard_err<T, E>(self) -> DiscardError<Self>
-	where
-		Self: Iterator<Item = Result<T, E>> + Sized,
-	{
-		DiscardError::new(self)
-	}
 }
