e8c83abfc95d054159b001e835ca116e88266ea7
wetfloo <yahahaha.1234567890@gmail.com>
2026-06-16 21:32:29 +0700

    use pretty-type-name for db service

--- src/lyrics/service/db.rs
+++ src/lyrics/service/db.rs
@@ -1,13 +1,25 @@
+use core::fmt;
+use std::any::type_name;
+
 use super::LyricsServiceError;
 use crate::lyrics::Lyrics;
 use crate::lyrics::TagData;
 use crate::lyrics::service::LyricsFetchService;
 use crate::lyrics::service::LyricsServiceResult;
 
-#[derive(Debug)]
-struct DbLyricsFetchService {}
+use pretty_type_name::pretty_type_name;
+
+struct DbLyricsFetchService {
+	db_conn: sqlite::ConnectionThreadSafe,
+}
 
 impl DbLyricsFetchService {}
+
+impl fmt::Debug for DbLyricsFetchService {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+		f.write_str(&pretty_type_name::<Self>())
+	}
+}
 
 struct DbLyricsResponse {}
 
